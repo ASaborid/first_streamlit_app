62607ae11d40bb0e5d1e7e4f261b124c2112cb91
Alberto Saborido Patiño <110113831+ASaborid@users.noreply.github.com>
2022-08-01 16:55:08 +0200

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json()) #just write the data to the screen
 
 # Let's Get the Fruityvice Data Looking a Little Nicer
